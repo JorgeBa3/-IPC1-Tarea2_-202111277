dbc9655f560b3d5774dc74affe226aab6f15a924
JorgeBa3 <49853820+JorgeBa3@users.noreply.github.com>
2022-08-25 17:29:00 -0600

    Tarea 2

---
+++ src/tarea2/Datos.java
@@ -0,0 +1,7 @@
+package tarea2;
+
+public class Datos {
+    String nombre="Jorge";
+    String apellido="De León";
+    String carnet="202111277";
+}
---
+++ src/tarea2/Tarea2.java
@@ -0,0 +1,12 @@
+
+package tarea2;
+public class Tarea2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
